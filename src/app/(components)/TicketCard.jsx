b75a4a820c496858d49c0e39310954ab702f9c79
VaibhavDangaich <31512333+VaibhavDangaich@users.noreply.github.com>
2025-06-09 15:52:13 +0530

    ---

--- src/app/(components)/TicketCard.jsx
+++ src/app/(components)/TicketCard.jsx
@@ -4,7 +4,22 @@ import PriorityDisplay from './PriorityDisplay'
 import ProgressDisplay from './ProgressDisplay'
 import StatusDisplay from './StatusDisplay'
 
-function TicketCard({ticket}) {
+function TicketCard({ ticket }) {
+  const timeStamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+
+    };
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleDateString("en-US", options);
+    return formattedDate.replace(/(\d+)\/(\d+)\/(\d+), (\d+):(\d+)/, "$2/$1/$3, $4:$5");
+    
+  }
   return (
     <div className='flex flex-col gap-3 bg-[#47566a] hover:bg-[#4f5e74] rounded-md shadow-lg p-3 m-2'>
     <div className='flex mb-3 justify-between'>
@@ -19,7 +34,7 @@ function TicketCard({ticket}) {
         
        <div className='flex mt-2'>
        <div className='flex flex-col'>
-          <p className='text-xs my-1'>{ticket.createdAt}</p>
+          <p className='text-xs my-1'>{timeStamp(ticket.createdAt)}</p>
           <ProgressDisplay progress={ticket.progress}></ProgressDisplay>
 
        </div>
